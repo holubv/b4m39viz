cda33d098a19fcca674f36955e99f14ddf96f00f
holubv <holubvojtech@gmail.com>
2022-05-12 01:29:56 +0200

    Add flight path rendering

--- src/js/airport.js
+++ src/js/airport.js
@@ -1,9 +1,148 @@
-export default class Airport {
+export class AirportMap {
+    /**
+     * @type {Airport[]}
+     */
+    airports = [];
+
+    /**
+     * @type {Flight[]}
+     */
+    flights = [];
+
+    /**
+     * @type {HTMLDivElement}
+     */
+    tooltipEl = null;
+
+    registerListeners() {
+        this.airports.forEach(airport => {
+            airport.el.addEventListener('mouseover', e => {
+                this.highlightAirports(airport);
+                this.highlightFlights(airport.flights);
+
+                let elBox = airport.el.getBoundingClientRect();
+
+                this.tooltipEl.style.display = 'block';
+                this.tooltipEl.style.top = (elBox.top - 18) + 'px';
+                this.tooltipEl.style.left = (elBox.left + elBox.width + 8) + 'px';
+                this.tooltipEl.textContent = airport.name;
+
+            });
+
+            airport.el.addEventListener('mouseout', e => {
+                this.removeHighlights();
+
+                this.tooltipEl.style.display = 'none';
+            });
+
+            airport.el.addEventListener('click', e => {
+                e.preventDefault();
+
+                this.airports.forEach(a => {
+                    a.el.style.display = null;
+                    if (a !== airport) {
+                        if (!a.hasDirectFlightConnection(airport)) {
+                            a.el.style.display = 'none';
+                        }
+                    }
+                });
+
+                this.flights.forEach(f => {
+                    f.el.style.display = null;
+                    f.el.classList.remove('selected');
+                    if (f.airport1 !== airport && f.airport2 !== airport) {
+                        f.el.style.display = 'none';
+                    } else {
+                        f.el.classList.add('selected');
+                    }
+                })
+            })
+        });
+    }
+
+    removeHighlights() {
+        this.highlightAirports(null);
+        this.highlightFlights(null);
+    }
+
+    highlightAirports(airports) {
+        if (!airports?.length) {
+            airports = [airports];
+        }
+
+        this.airports.forEach(a => {
+            if (airports.find(toA => toA?.id === a.id)) {
+                a.el.classList.add('highlight');
+            } else {
+                a.el.classList.remove('highlight');
+            }
+        });
+    }
+
+    highlightFlights(flights) {
+        if (!flights?.length) {
+            flights = [flights];
+        }
+
+        this.flights.forEach(f => {
+            if (flights.find(toF => f.equals(toF))) {
+                f.el.classList.add('highlight');
+            } else {
+                f.el.classList.remove('highlight');
+            }
+        });
+    }
+
+    async loadFromFile() {
+        let file = await import('bundle-text:/res/airlines.graphml');
+        let xml = new DOMParser().parseFromString(file, 'text/xml');
+
+        let graph = xml.querySelector('graph');
+        graph.querySelectorAll('node').forEach(node => {
+            this.airports.push(Airport.parseFromXML(node));
+        });
+
+        graph.querySelectorAll('edge').forEach(edge => {
+            let fromId = Number(edge.getAttribute('source'));
+            let toId = Number(edge.getAttribute('target'));
+
+            if (fromId < toId) {
+
+                let from = this.airports.find(Airport.byId(fromId));
+                let to = this.airports.find(Airport.byId(toId));
+
+                let flight = new Flight();
+                flight.airport1 = from;
+                flight.airport2 = to;
+
+                from.flights.push(flight);
+                to.flights.push(flight);
+                this.flights.push(flight);
+            }
+        });
+
+        let maxFlights = this.airports.reduce((acc, val) => {
+            if (val.flights.length > acc) {
+                return val.flights.length;
+            }
+            return acc;
+        }, 0);
+
+        this.airports.forEach(airport => {
+            airport.size = airport.flights.length / maxFlights;
+        });
+    }
+}
+
+export class Airport {
 
     id = -1;
     name = '';
     lat = 0;
     lon = 0;
+
+    x = 0;
+    y = 0;
 
     /**
      * @type {Flight[]}
@@ -11,6 +150,11 @@ export default class Airport {
     flights = [];
 
     size = 0;
+
+    /**
+     * @type {SVGCircleElement|null}
+     */
+    el = null;
 
     /**
      * @param {Element} node
@@ -36,6 +180,15 @@ export default class Airport {
         return airport => airport.id === id;
     }
 
+    hasDirectFlightConnection(airport) {
+
+        let id = airport.id;
+
+        return this.flights.some(f => {
+            return f.airport1.id === id || f.airport2.id === id;
+        });
+    }
+
 }
 
 export class Flight {
@@ -48,4 +201,13 @@ export class Flight {
      * @type {Airport}
      */
     airport2 = null;
+
+    /**
+     * @type {SVGPathElement|null}
+     */
+    el = null;
+
+    equals(f) {
+        return this.airport1.id === f?.airport1?.id && this.airport2.id === f?.airport2?.id;
+    }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import * as d3 from 'd3';
-import {default as Airport, Flight} from './airport';
-import {max} from "d3";
+import {AirportMap, Airport, Flight} from './airport';
 
 const width = document.body.clientWidth;
 const height = 600;
@@ -10,17 +9,19 @@ const projection = d3.geoAlbersUsa()
 
 const path = d3.geoPath(projection);
 
-const svg = d3.select('body')
+const svgWrapper = d3.select('body').append('div');
+
+const svg = svgWrapper
     .append('svg')
     .attr('width', width)
     .attr('height', height);
 
 const g = svg.append('g');
 
-const div = d3.select('body')
+const tooltipEl = svgWrapper
     .append('div')
     .attr('class', 'tooltip')
-    .style('opacity', 0);
+    .node();
 
 
 let zoom = d3.zoom()
@@ -30,100 +31,145 @@ let zoom = d3.zoom()
 
 svg.call(zoom);
 
+let forceEdgeBundle = () => {
+};
 
 const main = async () => {
 
+    {
+        // load force edge bundling library
+        window.d3 = {};
+        await import('../../lib/d3-ForceEdgeBundling');
+        forceEdgeBundle = window.d3.ForceEdgeBundling;
+        delete window.d3;
+    }
+
     const geoJson = await import('/res/gz_2010_us_040_00_20m.json');
+    drawMap(geoJson);
 
-    g.selectAll('path')
-        .data(geoJson.features)
-        .enter()
-        .append('path')
-        .attr('d', path)
-        .style('stroke', '#fff')
-        .style('stroke-width', '1')
-        .style('fill', d => {
+    const map = new AirportMap();
 
-            return 'rgb(213,222,217)';
-        });
+    await map.loadFromFile();
 
+    console.log(map.airports.find(a => a.flights.length === 0));
 
-    /**
-     * @type {Airport[]}
-     */
-    const airports = [];
+    map.tooltipEl = tooltipEl;
 
-    let file = await import('bundle-text:/res/airlines.graphml');
-    let xml = new DOMParser().parseFromString(file, 'text/xml');
-
-    let graph = xml.querySelector('graph');
-    graph.querySelectorAll('node').forEach(node => {
-        airports.push(Airport.parseFromXML(node));
+    map.airports.forEach(airport => {
+        // project lat lon to canvas x y
+        let p = projection([airport.lon, airport.lat]);
+        airport.x = p[0];
+        airport.y = p[1];
     });
 
-    graph.querySelectorAll('edge').forEach(edge => {
-        let fromId = Number(edge.getAttribute('source'));
-        let toId = Number(edge.getAttribute('target'));
+    drawFlights(map.airports, map.flights);
+    drawAirports(map.airports);
 
-        if (fromId < toId) {
+    map.registerListeners();
 
-            let from = airports.find(Airport.byId(fromId));
-            let to = airports.find(Airport.byId(toId));
-
-            let flight = new Flight();
-            flight.airport1 = from;
-            flight.airport2 = to;
-
-            from.flights.push(flight);
-            to.flights.push(flight);
-        }
-    });
-
-    //console.log(airports);
-
-    let maxFlights = airports.reduce((acc, val) => {
-        if (val.flights.length > acc) {
-            return val.flights.length;
-        }
-        return acc;
-    }, 0);
-
-    airports.forEach(airport => {
-        airport.size = airport.flights.length / maxFlights;
-    });
-
-    airports.sort((a, b) => b.size - a.size);
-
-    console.log(airports);
-
-    const airportSizeScale = d3.scaleLinear()
-        .domain([0, 1])
-        .range([1, 20])
-
-
-    g.selectAll('circle')
-        .data(airports)
-        .enter()
-        .append('circle')
-        .attr('cx', a => projection([a.lon, a.lat])[0])
-        .attr('cy', a => projection([a.lon, a.lat])[1])
-        .attr('r', a => {
-            return airportSizeScale(a.size);
-        })
-        .style('fill', "rgb(217,91,67)")
-        .style('opacity', 0.85)
-        .on('mouseover', (e, airport) => {
-            console.log(airport);
-            div.style('opacity', 1);
-            div.text(airport.name + ': ' + airport.flights.length)
-                .style('left', (e.pageX) + 'px')
-                .style('top', (e.pageY - 28) + 'px');
-        })
-        .on('mouseout', (e, airport) => {
-            div.style('opacity', 0);
-        });
-
+    console.log(map.airports);
 };
 
 main().then(() => {
 });
+
+function drawMap(geoJson) {
+    g.selectAll('path')
+        .data(geoJson.features)
+        .enter()
+        .append('path')
+        .attr('class', 'map')
+        .attr('d', path)
+        // .style('stroke', '#fff')
+        // .style('stroke-width', '1')
+        // .style('fill', d => {
+        //
+        //     return 'rgb(213,222,217)';
+        // });
+}
+
+function drawAirports(airports) {
+
+    let airportSizeSorted = [...airports];
+    airportSizeSorted.sort((a, b) => b.size - a.size);
+
+    const airportSizeScale = d3.scaleSqrt()
+        .domain([0, 1])
+        .range([1, 20]);
+
+    const airportSizeColorScale = d3.scaleSequential()
+        .domain([0, 1])
+        .interpolator(d3.interpolateCool);
+
+    g.selectAll('circle')
+        .data(airportSizeSorted)
+        .enter()
+        .append('circle')
+        .attr('class', 'airport')
+        .attr('cx', a => a.x)
+        .attr('cy', a => a.y)
+        .attr('r', a => {
+            return airportSizeScale(a.size);
+        })
+        .style('fill', a => airportSizeColorScale(a.size))
+        .style('stroke', a => airportSizeColorScale(a.size))
+        //.style('fill', 'rgb(217,91,67)')
+        //.style('opacity', 0.85)
+        // .on('mouseover', (e, airport) => {
+        //     console.log(airport);
+        //     div.style('opacity', 1);
+        //     div.text(airport.name + ': ' + airport.flights.length)
+        //         .style('left', (e.pageX) + 'px')
+        //         .style('top', (e.pageY - 28) + 'px');
+        // })
+        // .on('mouseout', (e, airport) => {
+        //     div.style('opacity', 0);
+        // })
+        .each((airport, index, d) => {
+            airports.find(Airport.byId(airport.id)).el = d[index];
+        });
+}
+
+/**
+ * @param {Airport[]} airports
+ * @param {Flight[]} flights
+ */
+function drawFlights(airports, flights) {
+
+    let nodes = airports.reduce((acc, curr) => {
+        acc[curr.id] = curr;
+        return acc;
+    }, {});
+
+    let edges = flights.map(f => {
+        return {
+            'source': f.airport1.id,
+            'target': f.airport2.id
+        };
+    });
+
+    let bundle = (forceEdgeBundle()
+        .nodes(nodes)
+        .edges(edges))();
+
+    let d3line = d3.line()
+        .x(d => d.x)
+        .y(d => d.y)
+        .curve(d3.curveLinear);
+
+    bundle.forEach((edgePoints, index) => {
+        // for each of the arrays in the results
+        // draw a line between the subdivions points for that edge
+        let path = g.append('path')
+            .attr('id', 'edge-' + index)
+            .attr('class', 'flight')
+            .attr('d', d3line(edgePoints))
+            //.style('stroke-width', 1)
+            //.style('stroke', '#bd22ff')
+            //.style('fill', 'none')
+            //.style('stroke-opacity', 0.15)
+            .node(); //use opacity as blending
+
+        flights[index].el = path;
+    });
+}
